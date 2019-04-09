bab9c6d5121642bd5a15fa7e317f0a59e13d13a0
toastal <toastal@protonmail.com>
2019-04-09 13:13:07 +0700

    js deps + better comments and make

--- src/Web/DOM/DOMParser.purs
+++ src/Web/DOM/DOMParser.purs
@@ -1,6 +1,6 @@
 module Web.DOM.DOMParser
   ( DOMParser
-  , newDOMParser
+  , makeDOMParser
   , parseFromString
   , parseHTMLFromString
   , parseSVGFromString
@@ -13,21 +13,25 @@ import Web.DOM.Document (Document)
 foreign import data DOMParser ∷ Type
 
 --| Create a new `DOMParser`
-foreign import newDOMParser ∷ Effect DOMParser
+foreign import makeDOMParser ∷ Effect DOMParser
 
+--| Parse a string with the first argumet being a string for a doctype
 foreign import parseFromString ∷ String → String → DOMParser → Document
 
---| Convience function to parse HTML from a string
+--| Convience function to parse HTML from a string, partially applying
+--| `parseFromString` with "text/html"
 parseHTMLFromString ∷ String → DOMParser → Document
 parseHTMLFromString =
   parseFromString "text/html"
 
---| Convience function to parse SVG from a string
+--| Convience function to parse SVG from a string, partially applying
+--| `parseFromString` with "image/svg+xml"
 parseSVGFromString ∷ String → DOMParser → Document
 parseSVGFromString =
   parseFromString "image/svg+xml"
 
---| Convience function to parse XML from a string
+--| Convience function to parse XML from a string, partially applying
+--| `parseFromString` with "application/xml"
 parseXMLFromString ∷ String → DOMParser → Document
 parseXMLFromString =
   parseFromString "application/xml"
